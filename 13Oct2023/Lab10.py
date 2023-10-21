d10170274a92a1d6731b81df80e84549769324b2
pandiyas <pandiyas@gmail.com>
2023-10-20 20:24:47 -0400

    first

---
+++ 13Oct2023/Lab10.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the Value A:\n"))
+b = int(input("Enter the Value A:\n"))
+
+print(a==b)
+print(a<b)
+print(a>b)
---
+++ 13Oct2023/Lab11.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the Value A:\n"))
+b = int(input("Enter the Value A:\n"))
+a+=2
+print("the value",a)
+
+x=[1,2,3]
+y=[1,2,3]
+print(x is y)
+
+a=10
+print(-a)
